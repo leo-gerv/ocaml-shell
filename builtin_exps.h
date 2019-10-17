8a41df4b7ad6ab8dfe4d61c632f03bc2ae081807
leo-gerv <55334746+leo-gerv@users.noreply.github.com>
2019-10-17 22:19:41 +0200

    Update builtin_exps.h

--- builtin_exps.h
+++ builtin_exps.h
@@ -6,8 +6,7 @@
 namespace BuiltIn_Expressions {
     static QStringList modules({});
     static int modules_count = modules.length();
-    static QStringList functions({"int_of_string","string_of_int", "float_of_int", "int_of_float","string_of_float","float_of_string"
-                                 ,"open","#load","#use","let",
+    static QStringList functions({"#load","#use",
                                   "Unix","Unix.sleep","\"unix.cma\"",
                                   "Random","Random.init","Random.int",
                                   "on",
